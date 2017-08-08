140d1f1fd59162db830b54bc08e9b75762c1977a
pvp23 <patrick.v.platen@gmail.com>
2017-08-08 22:57:21 +0200

    a

---
+++ src/Lesson1/Calculator.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+import sun.management.counter.Variability;
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		int a = ;//fill in
+		int b = ;//fill in 
+		
+		int result = ;// sum of a and b
+		System.out.println(result);
+	}
+
+}

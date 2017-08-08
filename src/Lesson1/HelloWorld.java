8ca707896a9b85f0eb050fdbc3c9672bc5d7b8f4
pabliiito <30844516+pabliiito@users.noreply.github.com>
2017-08-08 22:54:39 +0200

    finish

--- src/Lesson1/HelloWorld.java
+++ src/Lesson1/HelloWorld.java
@@ -1,7 +1,9 @@
 package Lesson1;
 
 public class HelloWorld {
-
+	public static void main(String[] args) {
 	//Task: Print out the words "Hello World"
-	
+
+	System.out.println("Hello World!");
+}
 }

9d04f5014c81cdae334ce63067ef2f99464be550
pabliiito <30844516+pabliiito@users.noreply.github.com>
2017-08-08 23:27:45 +0200

    finish

--- src/Lesson1/CalculaterMultiplication.java
+++ src/Lesson1/CalculaterMultiplication.java
@@ -1,7 +1,21 @@
 package Lesson1;
 
+import java.util.Scanner;
+
 public class CalculaterMultiplication {
 	
+	public static void main(String[] args) {
 	//build the same Calculator as in sum only multiplicator now
 
+		
+		Scanner in = new Scanner (System.in);
+		System.out.println("Inserta el numero a");
+		int a = in.nextInt();
+		
+		System.out.println("Inserta el numero b");
+		int b = in.nextInt();
+		
+		int result = a * b;//Multiplicacion de a por b
+		System.out.println("El resultado es " + result);
+	}
 }

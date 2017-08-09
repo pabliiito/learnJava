0ea73d296512b2fec9861abc81dcf72330ed2289
pabliiito <30844516+pabliiito@users.noreply.github.com>
2017-08-09 18:40:22 +0200

    Finish to calculator division

--- src/Lesson1/CalculatorDivision.java
+++ src/Lesson1/CalculatorDivision.java
@@ -1,8 +1,29 @@
 package Lesson1;
 
+import java.util.Scanner;
+
+
 public class CalculatorDivision {
 	
 	//build the same as in sum BUT: be careful with int, double, ... when dividing. 
 	// 5 / 2 is not a int !!!!
+	
+	public static void main(String[] args) {
+	//build the same Calculator as in sum only multiplicator now
 
+		 
+		
+		Scanner in = new Scanner (System.in);
+		System.out.print("Inserta el numero a");
+		double a = in.nextInt();
+		
+		
+		
+		System.out.println("Ingrese el primer numero");
+		double b = in.nextInt();
+		
+
+		double result = (a / b);//Multiplicacion de a por b
+		System.out.println("El resultado es " + result);
+	}
 }
